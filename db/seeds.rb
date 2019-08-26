f9227aa1747df75d62557be170dbf6858b33e7ac
carrjona <carrjona@gitlab.ecs.vuw.ac.nz>
2019-08-26 21:36:15 +1200

    Add model and seeds for Mitre 10 Cup teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,92 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Initialise Mitre 10 Cup teams
+
+Team.create([
+    {
+        name: 'Tasman',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Auckland',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Waikato',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'North Harbour',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Counties-Manukau',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Wellington',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Canterbury',
+        conference: 'Premiership',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Taranaki',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Hawke\'s Bay',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Bay of Plenty',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Otago',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Northland',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Manawatu',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+    {
+        name: 'Southland',
+        conference: 'Championship',
+        tournament: 'Mitre 10 Cup',
+        country: 'New Zealand',
+    },
+])
