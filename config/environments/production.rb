e0d9eed6c02b5e2dab4c33b5aeb6ed0d0ab7d9c2
jonathancarr <jonathancnz@hotmail.com>
2019-08-28 21:44:26 +1200

    Allow all origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'
+      origins '*'
       resource '*', :headers => :any, :methods => [:get, :post, :options]
     end
   end
